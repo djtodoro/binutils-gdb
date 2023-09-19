5b0923e9f55182d2f56c7e5775d5660d8cd974a5
Aleksandar Rikalo <arikalo@gmail.com>
2023-09-19 18:21:55 +0200

    Testcase for --relax-balc-trampolines

---
+++ gold/testsuite/nanomips_balc_relax.s
@@ -0,0 +1,37 @@
+.linkrelax
+	.module	pcrel
+	.section	.text.startup,"ax",@progbits
+	.align	1
+
+	.globl	test
+	.ent	test
+test:
+	balc	subroutine
+	balc	subroutine
+	balc	subroutine
+	balc	subroutine
+	balc	subroutine
+	balc	subroutine
+	.rept 250
+	nop[32]
+	.endr
+	balc	subroutine
+	balc	subroutine
+	balc	subroutine
+	balc	subroutine
+	balc	subroutine
+	balc	subroutine
+	.rept 512
+	nop[32]
+	.endr
+	jrc	$ra
+	.end	test
+	.size	test, .-test
+
+	.globl	subroutine
+	.ent	subroutine
+subroutine:
+	nop
+	jrc	$ra
+	.end	subroutine
+	.size	subroutine, .-subroutine
